369b9de2a415d53e5ea2d5a1d1b81093fe15037b
barbudreadmon <barbudreadmon@gmail.com>
2021-04-17 18:25:51 +0200

    varying VER_ALPHA

--- src/burn/version.h
+++ src/burn/version.h
@@ -4,7 +4,9 @@
 #define VER_MAJOR  1
 #define VER_MINOR  0
 #define VER_BETA  0
-#define VER_ALPHA 1
+#ifndef VER_ALPHA
+	#define VER_ALPHA 0
+#endif
 
 #define BURN_VERSION (VER_MAJOR * 0x100000) + (VER_MINOR * 0x010000) + (((VER_BETA / 10) * 0x001000) + ((VER_BETA % 10) * 0x000100)) + (((VER_ALPHA / 10) * 0x000010) + (VER_ALPHA % 10))
 
